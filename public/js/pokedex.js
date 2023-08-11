938bc2fc1ba4c69e60550be99495925340ed7fb5
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-08-10 21:29:06 -0700

    update to pokedex, added Search bar and centered each gif

--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -2,7 +2,7 @@ const pokedex = document.querySelector('.pokedex');
 
 async function fetchPokemon() {
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=802');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=890');
     const data = await response.json();
     const pokemonList = data.results;
 
@@ -24,20 +24,21 @@ async function fetchPokemonDetails(pokemon) {
   }
 }
 
-// ... (previous code)
-
 function displayPokemon(pokemon) {
     const pokemonDiv = document.createElement('div');
     pokemonDiv.classList.add('pokemon');
   
+    const displayName = pokemon.name.replace(/-/g, ' '); // Replace hyphens with spaces for the URL
+    const nameForUrl = displayName.toLowerCase().replace(/ /g, '_'); // Replace spaces with underscores for the URL
+  
     // Create an <a> element with an <img> child for the Pokémon image
     const pokemonLink = document.createElement('a');
-    pokemonLink.href = `https://projectpokemon.org/images/normal-sprite/${pokemon.name.toLowerCase()}.gif`;
+    pokemonLink.href = `https://projectpokemon.org/images/normal-sprite/${nameForUrl}.gif`;
     pokemonLink.target = '_blank'; // Open link in a new tab
     const pokemonImage = document.createElement('img');
   
     // Set the image source
-    pokemonImage.src = `https://projectpokemon.org/images/normal-sprite/${pokemon.name.toLowerCase()}.gif`;
+    pokemonImage.src = `https://projectpokemon.org/images/normal-sprite/${nameForUrl}.gif`;
   
     // Append the image to the link
     pokemonLink.appendChild(pokemonImage);
@@ -47,10 +48,11 @@ function displayPokemon(pokemon) {
   
     // Create a <p> element for the Pokémon name at the bottom
     const pokemonName = document.createElement('p');
-    pokemonName.textContent = pokemon.name;
+    pokemonName.textContent = pokemon.name.replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase());; // Replace hyphens with spaces AND captailize first letter for the Names
     pokemonDiv.appendChild(pokemonName);
   
     pokedex.appendChild(pokemonDiv);
   }
-
-fetchPokemon();
+  
+  fetchPokemon();
+  
